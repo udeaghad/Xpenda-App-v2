3fa5b8cce5681bf0a6aedea2488c339fed16bcda
udeaghad <dozie.udeagha@gmail.com>
2022-10-21 15:41:41 +0100

    Setup user associations andvalidation

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :expenditures, dependent: :destroy, foreign_key: 'user_id'
+  has_many :categories, dependent: :destroy, foreign_key: 'user_id'
+
+  validates :name, presence: true, length: { maximum: 100 }
+
 end
