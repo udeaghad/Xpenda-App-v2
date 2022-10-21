f905674692a42007e9fab05777d1d752c0b4bee6
udeaghad <dozie.udeagha@gmail.com>
2022-10-21 16:06:46 +0100

    Setup routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,18 @@
 Rails.application.routes.draw do
   devise_for :users
+
+  devise_scope :user do 
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  root "home#index"
+
+  resources :categories, only: [:index, :new, :create, :edit, :update, :destroy] do
+    resources :category_expenditures, only: [:index]
+  end
+
+  resources :expenditures, only: [:index, :new, :create, :destroy] 
 end
